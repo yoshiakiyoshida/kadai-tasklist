1140dad221cada851c671cbcc6b8fa1f48a30eb8
yoshiaki.yoshida <yoshiaki.yoshida@techacademy.jp>
2019-12-07 11:06:57 +0000

    kadai-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_11_30_151347) do
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
