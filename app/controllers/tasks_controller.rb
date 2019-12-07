1a7d62730986780a1bc28af1a58b96e001fe5470
yoshiaki.yoshida <yoshiaki.yoshida@techacademy.jp>
2019-12-07 12:07:41 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,11 +40,11 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @message = Message.find(params[:id])
-    @message.destroy
+    @task = Task.find(params[:id])
+    @task.destroy
 
-    flash[:success] = 'Message は正常に削除されました'
-    redirect_to messages_url
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
   
   def task_params
